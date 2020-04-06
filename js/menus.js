5bd0ba68bab6611a6a8a64205ab18e9d2f4d93f8
devdevil-production <saudd2gh@gmail.com>
2020-04-06 13:49:39 +0400

    Create menus.js file + Add menu items

---
+++ js/menus.js
@@ -0,0 +1,107 @@
+const menus = [
+  {
+    name: "breakfast",
+    items: [
+      {
+        name: "Pancakes",
+        price: 12.9,
+        img: "#!",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Poached Eggs with Baked Beans",
+        price: 12.9,
+        img: "food-coffee-breakfast-103124",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Breakfast Cereal",
+        price: 12.9,
+        img: "blueberries-breakfast-cereals-food-4815",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Scrambled Egg with Toasted Bread",
+        price: 12.9,
+        img: "toaster-bread-scrambled-eggs-grilled-tomato-and-sausage-693268",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+    ],
+  },
+  {
+    name: "lunch",
+    items: [
+      {
+        name: "Macaroni",
+        price: 12.9,
+        img: "closes-up-photo-of-macaroni-1437267",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Tortellini",
+        price: 12.9,
+        img: "close-up-photo-of-tortellini-2412950",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Veggi Pizza",
+        price: 12.9,
+        img: "food-on-white-ceramic-plate-near-cloth-1435903",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Pasta with Parmasan",
+        price: 12.9,
+        img: "close-up-photo-of-risotto-dish-921362",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Risotto Dish",
+        price: 12.9,
+        img: "close-up-photo-of-risotto-dish-921362",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Vegetable Soup",
+        price: 12.9,
+        img: "soup-with-vegetables-on-white-ceramic-bowl-3493579",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+    ],
+  },
+  {
+    name: "dinner",
+    items: [
+      {
+        name: "Beaf Pizza",
+        price: 12.9,
+        img: "pizza-on-brown-wooden-table-3768007",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Hamburger Plate",
+        price: 12.9,
+        img: "close-up-photo-of-hamburger-on-plate-3821695",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Cheesy Pizza",
+        price: 12.9,
+        img: "close-up-photo-of-tortellini-2412950",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Seafood Pasta",
+        price: 12.9,
+        img: "cooked-shrimp-with-noodles-725997",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+      {
+        name: "Fries and Steak",
+        price: 12.9,
+        img: "fries-and-steak-on-white-ceramic-plate-3821692",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut magni eaque.`,
+      },
+    ],
+  },
+];
